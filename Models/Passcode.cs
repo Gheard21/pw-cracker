34bddeadd19e666dc275f6beb268935ffd415cd4
Gheard21 <gregoryheard18@gmail.com>
2021-03-22 14:17:13 +0000

    add comments

--- Models/Passcode.cs
+++ Models/Passcode.cs
@@ -19,6 +19,10 @@ namespace EsetChallenge.Models
             Array.Sort(Digits);
         }
 
+        /*
+            Get all the unique characters from the file, and for each one new up a Digit object to record all
+            the elements that we find when processing the hints that come prior to the one we're currently processing.
+        */
         public void InitDigits() => Digits = FileText.Replace(Environment.NewLine, "").Distinct().Select(d => new Digit(d)).ToArray();
 
         public void PrintResults()
@@ -29,6 +33,11 @@ namespace EsetChallenge.Models
             Console.WriteLine(Environment.NewLine);
         }
 
+        /*
+            Seperate the file into individual hints that are n long. As long as it's not the first element in
+            the hint, then add each previous element to the PreviousNumbers hash set for the current item in
+            Digits.
+        */
         private void ProcessHints() => Array.ForEach(FileText.Split(Environment.NewLine), hint =>
         {
             for (var i = 1; i < hint.Length; i++)
